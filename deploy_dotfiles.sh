ac52e18997d0d45d6a7985498258678c49b95e80
Tatsuya Furuoka <t.furuoka810@gmail.com>
2016-08-06 05:29:10 +0900

    Create initial deploy_dotfiles.sh

---
+++ deploy_dotfiles.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+function link_dotfile () {
+    conffile=$1
+    if [ -f ~/${conffile} -o -L ~/${conffile} ]; then 
+        echo -n "Is it ok to overwrite existing ${conffile} [Y/N]? > "
+        read INPUT
+        if [ ${INPUT} = "Y" -o ${INPUT} = "y" ]; then
+            ln -sf $PWD/${conffile} ~/${conffile}
+        elif [ ${INPUT} = "N" -o ${INPUT} = "n" ]; then
+            echo "Canceled: deploy ${conffile}"
+        else
+            echo "Enter Y or N"
+        fi
+    else 
+        ln -s $PWD/${conffile} ~/${conffile}
+    fi
+}
+
+# Deploy .tmux.conf
+link_dotfile ".tmux.conf"
